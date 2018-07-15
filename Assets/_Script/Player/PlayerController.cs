ffbda78c2b59c673722d1e618e37ab17010e524a
keel-210 <crafters_touhou@yahoo.co.jp>
2018-07-15 11:26:21 +0900

    Initial commit

---
+++ Assets/_Script/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] float Speed;
+	Rigidbody2D rb;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetAxis ("Horizontal") != 0)
+		{
+			rb.velocity = new Vector2 (Input.GetAxisRaw ("Horizontal") * Speed, rb.velocity.y);
+		}
+		else
+		{
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+	}
+	void FixedUpdate ()
+	{
+
+	}
+}
