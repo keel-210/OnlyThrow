e21f35105f73aa13d44407921d3da8e614bc2441
keel-210 <crafters_touhou@yahoo.co.jp>
2018-07-17 17:04:11 +0900

    Ver0.02

--- Assets/_Script/Enemy/TestZakoController.cs
+++ Assets/_Script/Enemy/TestZakoController.cs
@@ -11,49 +11,51 @@ public class TestZakoController : MonoBehaviour, Health, IThrowable
 	Collider2D col;
 	public Vector2 velocityCast, CatchedPos;
 	bool IsCatched, IsThrowed;
-	void Start ()
+	void Start()
 	{
-		rb = GetComponent<Rigidbody2D> ();
-		col = GetComponent<Collider2D> ();
+		rb = GetComponent<Rigidbody2D>();
+		col = GetComponent<Collider2D>();
 	}
-	void FixedUpdate ()
+	void FixedUpdate()
 	{
 		if (IsCatched)
 		{
 			transform.localPosition = CatchedPos;
 		}
 	}
-	public void Catch (Transform player)
+	public void Catch(Transform player)
 	{
-		Debug.Log ("Has Catched");
+		Debug.Log("Has Catched");
 		transform.parent = player;
 		CatchedPos = transform.localPosition;
 		rb.gravityScale = 0;
 		IsCatched = true;
 	}
-	public void Throw (Vector2 Direction)
+	public void Throw(Vector2 Direction)
 	{
 		IsCatched = false;
 		IsThrowed = true;
 		gameObject.layer = 13;
+		transform.parent = null;
 		rb.velocity = Direction * ThrowedSpeed;
 		velocityCast = rb.velocity;
 		rb.gravityScale = 0;
 	}
-	void OnCollisionEnter2D (Collision2D obj)
+	void OnCollisionEnter2D(Collision2D obj)
 	{
 		if (IsThrowed)
 		{
-			IReflectable IRef = obj.gameObject.GetComponent<IReflectable> ();
+			IReflectable IRef = obj.gameObject.GetComponent<IReflectable>();
 			if (IRef != null)
 			{
-				rb.velocity = IRef.Reflect (rb, velocityCast);
+				Debug.Log(obj.gameObject);
+				IRef.Reflect(rb, velocityCast);
 				velocityCast = rb.velocity;
 			}
-			IDamagable IDam = obj.gameObject.GetComponent<IDamagable> ();
+			IDamagable IDam = obj.gameObject.GetComponent<IDamagable>();
 			if (IDam != null)
 			{
-				IDam.Damage ();
+				IDam.Damage();
 			}
 		}
 	}
--- Assets/_Script/StageGimmics/ReflectWall.cs
+++ Assets/_Script/StageGimmics/ReflectWall.cs
@@ -1,29 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ReflectWall : MonoBehaviour, IReflectable
 {
 	[SerializeField] LayerMask mask;
 
-	public Vector2 Reflect (Rigidbody2D rb, Vector2 Cast)
+	public Vector2 Reflect(Rigidbody2D rb, Vector2 Cast)
 	{
 		Vector2 ReflectVec = Vector2.zero;
-		Vector2 hitUp = Physics2D.Raycast (rb.position, Vector2.up, 100f, mask).point - rb.position;
-		Vector2 hitDown = Physics2D.Raycast (rb.position, Vector2.down, 100f, mask).point - rb.position;
-		Vector2 hitRight = Physics2D.Raycast (rb.position, Vector2.right, 100f, mask).point - rb.position;
-		Vector2 hitLeft = Physics2D.Raycast (rb.position, Vector2.left, 100f, mask).point - rb.position;
-		Vector2 contactPoint = new Vector2 (Mathf.Abs (hitRight.x) < Mathf.Abs (hitLeft.x) ? hitRight.x : hitLeft.x, Mathf.Abs (hitUp.y) < Mathf.Abs (hitDown.y) ? hitUp.y : hitDown.y);
-		Debug.Log (contactPoint.ToString ("F5"));
-		if (Mathf.Abs (contactPoint.x) < Mathf.Abs (contactPoint.y))
+		RaycastHit2D hitUp = Physics2D.Raycast(rb.position, Vector2.up, 100f, mask);
+		RaycastHit2D hitDown = Physics2D.Raycast(rb.position, Vector2.down, 100f, mask);
+		RaycastHit2D hitRight = Physics2D.Raycast(rb.position, Vector2.right, 100f, mask);
+		RaycastHit2D hitLeft = Physics2D.Raycast(rb.position, Vector2.left, 100f, mask);
+
+		if (hitUp && hitDown && hitRight && hitLeft)
 		{
-			ReflectVec = new Vector2 (-Cast.x, Cast.y);
+			Vector2 up, down, right, left;
+			up = hitUp.point - rb.position;
+			down = hitDown.point - rb.position;
+			right = hitRight.point - rb.position;
+			left = hitLeft.point - rb.position;
+			Vector2 contactPoint = new Vector2(Mathf.Abs(right.x) < Mathf.Abs(left.x) ? right.x : left.x,
+															 Mathf.Abs(up.y) < Mathf.Abs(down.y) ? up.y : down.y);
+			Debug.Log(contactPoint.ToString("F5"));
+			if (Mathf.Abs(contactPoint.x) < Mathf.Abs(contactPoint.y))
+			{
+				ReflectVec = new Vector2(-Cast.x, Cast.y);
+			}
+			else
+			{
+				ReflectVec = new Vector2(Cast.x, -Cast.y);
+			}
+			rb.velocity = ReflectVec;
+			iTween.ShakePosition(Camera.main.gameObject, new Vector2(0.2f, 0.2f), 0.1f);
 		}
 		else
 		{
-			ReflectVec = new Vector2 (Cast.x, -Cast.y);
+			Debug.Log("Pos" + rb.position);
+			Debug.Log("Pos" + rb.transform.position);
+			Debug.Log("Up" + Physics2D.Raycast(rb.position, Vector2.up, 100f, mask).point.ToString("F5"));
+			Debug.Log("Down" + Physics2D.Raycast(rb.position, Vector2.down, 100f, mask).point.ToString("F5"));
+			Debug.Log("Right" + Physics2D.Raycast(rb.position, Vector2.right, 100f, mask).point.ToString("F5"));
+			Debug.Log("Left" + Physics2D.Raycast(rb.position, Vector2.left, 100f, mask).point.ToString("F5"));
+			EditorApplication.isPaused = true;
 		}
-		iTween.ShakePosition (Camera.main.gameObject, new Vector2 (0.2f, 0.2f), 0.1f);
 		return ReflectVec;
 	}
 }
